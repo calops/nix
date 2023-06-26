993755aa3e8964ee45246710e90473c16eba892b
Rémi Labeyrie <calops@tocards.net>
2023-06-27 01:25:26 +0200

    stockly devenv

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Stockly dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+    };
+  in {
+    devShells.x86_64-linux.stockly = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
+      buildInputs = with pkgs; [
+        bzip2
+        cargo
+        clang
+        cmake
+        gcc
+        libclang
+        libllvm
+        openssl
+        rust-analyzer
+        rustPlatform.bindgenHook
+        rustc
+        zlib
+      ];
+    };
+  };
+}
