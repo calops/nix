2e7f1ec0e27a63aa54352b84ad794cc1891b5e39
Rémi Labeyrie <calops@tocards.net>
2024-03-26 12:35:34 +0100

    fix element with new electron version

--- modules/home/config/programs/element/default.nix
+++ modules/home/config/programs/element/default.nix
@@ -10,7 +10,7 @@
 
   # Electron doesn't play nice with the Wayland/NVidia combo
   pkg = pkgs.writeShellScriptBin "element-desktop" ''
-    NIXOS_OZONE_WL=1 ${lib.getExe pkgs.element-desktop} --use-gl=desktop
+    NIXOS_OZONE_WL=0 ${lib.getExe pkgs.element-desktop} --ozone-platform-hint=auto
   '';
 in
   with lib; {
