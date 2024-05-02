180fb26af4c1792b578eef0d40ae2bbe05d6ecd7
Rémi Labeyrie <calops@tocards.net>
2024-05-02 10:43:32 +0200

    feat(neovim): refactor LSP configuration
    The LSP configuration for Neovim has been refactored for better
    readability and maintainability. The initialization and configuration
    functions for LSP have been extracted into separate functions. This
    change also includes the removal of redundant Neovim lua LSP utilities
    configuration.

--- modules/home/config/programs/neovim/config/lua/plugins/lsp.lua
+++ modules/home/config/programs/neovim/config/lua/plugins/lsp.lua
@@ -1,9 +1,103 @@
 local map = require("core.utils").map
 
+local lsp_init_function = function()
+	map {
+		K = { vim.lsp.buf.hover, "Show documentation" },
+		H = { function() vim.diagnostic.open_float { border = "rounded" } end, "Show diagnostics" },
+		["<C-k>"] = { vim.lsp.buf.signature_help, "Interactive signature help" },
+		["<leader>r"] = {
+			name = "refactor",
+			n = { vim.lsp.buf.rename, "Interactive rename" },
+			f = { vim.lsp.buf.format, "Format code" },
+		},
+		["<leader>a"] = { vim.lsp.buf.code_action, "Interactive list of code actions", mode = { "n", "v" } },
+		["<leader>i"] = {
+			function()
+				---@diagnostic disable-next-line: inject-field
+				vim.b.inlay_hints_enabled = not vim.b.inlay_hints_enabled
+				vim.lsp.inlay_hint.enable(vim.b.inlay_hints_enabled or false)
+			end,
+			"Toggle inlay hints for buffer",
+		},
+	}
+
+	vim.api.nvim_create_autocmd("InsertEnter", { callback = function() vim.lsp.inlay_hint.enable(false) end })
+
+	vim.api.nvim_create_autocmd(
+		"InsertLeave",
+		{ callback = function() vim.lsp.inlay_hint.enable(vim.b.inlay_hints_enabled or false) end }
+	)
+
+	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+	vim.lsp.handlers["textDocument/signatureHelp"] =
+		vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+end
+
+local lsp_config_function = function()
+	require("neoconf")
+	require("neodev").setup {
+		pathStrict = false,
+		-- Always load nvim plugins for lua_ls, this is a temporary hack
+		-- FIXME: this hurts performance, should be fixed upstream
+		override = function(_, library)
+			library.enabled = true
+			library.plugins = true
+			library.types = true
+			library.runtime = true
+		end,
+	}
+
+	require("lsp-zero")
+	require("mason").setup { ui = { border = "rounded" } }
+	require("mason-lspconfig").setup {
+		automatic_installation = false,
+		-- handlers = { lsp_zero.default_setup },
+	}
+
+	local lspconfig = require("lspconfig")
+	lspconfig.vtsls.setup {
+		settings = {
+			typescript = {
+				inlayHints = {
+					enumMemberValues = { enabled = true },
+					functionLikeReturnTypes = { enabled = true },
+					parameterNames = { enabled = "all" },
+					parameterTypes = { enabled = false },
+					propertyDeclarationTypes = { enabled = true },
+					variableTypes = { enabled = true },
+				},
+			},
+		},
+	}
+	lspconfig.nixd.setup {
+		on_init = function(client, _)
+			-- Turn off semantic tokens until they're more consistent
+			client.server_capabilities.semanticTokensProvider = nil
+		end,
+	}
+	lspconfig.nil_ls.setup {}
+	lspconfig.lua_ls.setup {
+		settings = {
+			Lua = {
+				format = { enable = false },
+				hint = { enable = true },
+			},
+		},
+	}
+	lspconfig.lexical.setup {
+		filetypes = { "elixir", "eelixir", "heex" },
+		cmd = { "lexical" },
+		root_dir = function(fname)
+			return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
+		end,
+	}
+end
+
 return {
 	{
 		"VonHeikemen/lsp-zero.nvim",
 		lazy = false,
+		priority = 50,
 		dependencies = {
 			"neovim/nvim-lspconfig",
 			"williamboman/mason-lspconfig.nvim",
@@ -11,93 +105,8 @@ return {
 			"onsails/lspkind.nvim",
 			"folke/neodev.nvim",
 		},
-		init = function()
-			map {
-				K = { vim.lsp.buf.hover, "Show documentation" },
-				H = { function() vim.diagnostic.open_float { border = "rounded" } end, "Show diagnostics" },
-				["<C-k>"] = { vim.lsp.buf.signature_help, "Interactive signature help" },
-				["<leader>r"] = {
-					name = "refactor",
-					n = { vim.lsp.buf.rename, "Interactive rename" },
-					f = { vim.lsp.buf.format, "Format code" },
-				},
-				["<leader>a"] = { vim.lsp.buf.code_action, "Interactive list of code actions", mode = { "n", "v" } },
-				["<leader>i"] = {
-					function()
-						---@diagnostic disable-next-line: inject-field
-						vim.b.inlay_hints_enabled = not vim.b.inlay_hints_enabled
-						vim.lsp.inlay_hint.enable(vim.b.inlay_hints_enabled or false)
-					end,
-					"Toggle inlay hints for buffer",
-				},
-			}
-
-			vim.api.nvim_create_autocmd("InsertEnter", { callback = function() vim.lsp.inlay_hint.enable(false) end })
-
-			vim.api.nvim_create_autocmd(
-				"InsertLeave",
-				{ callback = function() vim.lsp.inlay_hint.enable(vim.b.inlay_hints_enabled or false) end }
-			)
-
-			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-			vim.lsp.handlers["textDocument/signatureHelp"] =
-				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-		end,
-		config = function()
-			require("neodev")
-			require("neoconf")
-
-			local lsp_zero = require("lsp-zero")
-
-			require("mason").setup { ui = { border = "rounded" } }
-			require("mason-lspconfig").setup {
-				automatic_installation = false,
-				handlers = {
-					lsp_zero.default_setup,
-					vtsls = function()
-						require("lspconfig").vtsls.setup {
-							settings = {
-								typescript = {
-									inlayHints = {
-										enumMemberValues = { enabled = true },
-										functionLikeReturnTypes = { enabled = true },
-										parameterNames = { enabled = "all" },
-										parameterTypes = { enabled = false },
-										propertyDeclarationTypes = { enabled = true },
-										variableTypes = { enabled = true },
-									},
-								},
-							},
-						}
-					end,
-				},
-			}
-
-			-- Installed by nix
-			local lspconfig = require("lspconfig")
-			lspconfig.nixd.setup {
-				on_init = function(client, _)
-					-- Turn off semantic tokens until they're more consistent
-					client.server_capabilities.semanticTokensProvider = nil
-				end,
-			}
-			lspconfig.nil_ls.setup {}
-			lspconfig.lua_ls.setup {
-				settings = {
-					Lua = {
-						format = { enable = false },
-						hint = { enable = true },
-					},
-				},
-			}
-			lspconfig.lexical.setup {
-				filetypes = { "elixir", "eelixir", "heex" },
-				cmd = { "lexical" },
-				root_dir = function(fname)
-					return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
-				end,
-			}
-		end,
+		init = lsp_init_function,
+		config = lsp_config_function,
 	},
 	-- Rust-specific utilities and LSP configurations
 	{
@@ -137,23 +146,6 @@ return {
 				},
 			}
 		end,
-	},
-	-- Neovim lua LSP utilities
-	{
-		"folke/neodev.nvim",
-		lazy = false,
-		enabled = true,
-		opts = {
-			pathStrict = false,
-			-- Always load nvim plugins for lua_ls, this is a temporary hack
-			-- FIXME: this hurts performance, should be fixed upstream
-			override = function(_, library)
-				library.enabled = true
-				library.plugins = true
-				library.types = true
-				library.runtime = true
-			end,
-		},
 	},
 	-- AI, baby
 	{
