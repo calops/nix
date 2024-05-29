fcb4c039103be31705938a88446bb409318dbe3d
Rémi Labeyrie <calops@tocards.net>
2024-05-29 13:20:19 +0200

    refactor(nixos): restructure module configuration files
    This commit restructures the NixOS module configuration files. The
    default.nix files in the config subdirectories have been renamed and
    moved up one level for clarity and ease of access.
    
    Additionally, the default.nix file in the config directory has been
    deleted, and its contents have been moved to the default.nix file in the
    nixos directory. This change simplifies the structure and makes the
    configuration more straightforward.
    
    This restructuring should make the configuration easier to navigate and
    maintain.

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,56 @@
-{ inputs, ... }:
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ../common
     inputs.stylix.nixosModules.stylix
-  ];
+  ] ++ lib.snowfall.get-non-default-nix-files ./.;
+
+  config = {
+    system.stateVersion = "24.05";
+    hardware.enableAllFirmware = true;
+
+    nix = {
+      optimise.automatic = true;
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+    };
+
+    boot.kernel.sysctl = {
+      "fs.inotify.max_user_watches" = 100000;
+      "fs.inotify.max_queued_events" = 100000;
+    };
+
+    console = {
+      font = "ter-124b";
+      keyMap = lib.mkDefault "fr";
+      packages = [ pkgs.terminus_font ];
+    };
+
+    stylix.homeManagerIntegration.autoImport = false;
+
+    services.udisks2.enable = true;
+    services.openssh.enable = true;
+
+    programs.fish.enable = true;
+    programs.mtr.enable = true;
+    programs.gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+
+    # Support for dynamic linking in NixOS
+    programs.nix-ld.enable = true;
+    environment.sessionVariables = {
+      NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [ ];
+      NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+    };
+  };
 }
