6cb6ecef6aaf75accb388fb333c37af3b6df07a8
Rémi Labeyrie <calops@tocards.net>
2024-07-02 11:01:57 +0200

    add cache sources

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,19 +1,26 @@
 { pkgs, lib, ... }:
 {
-  nix.package = lib.mkForce pkgs.nixVersions.latest; # TODO: remove once snowfall is fixed
+  nix.package = lib.mkForce pkgs.nixVersions.latest;
   nix.settings = {
     experimental-features = [
       "flakes"
       "nix-command"
     ];
-    substituters = [
+
+    extra-substituters = [
+      "https://nix-ai-stuff.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+      "https://ai.cachix.org"
       "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
     ];
-    trusted-public-keys = [
+
+    extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix-ai-stuff.cachix.org-1:WlUGeVCs26w9xF0/rjyg32PujDqbVMlSHufpj1fqix8="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
     ];
+
     trusted-users = [
       "root"
       "@wheel"
