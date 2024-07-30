b728438086ac14f370ff9f7c696aa032f31b89cb
Rémi Labeyrie <calops@tocards.net>
2024-07-30 12:08:41 +0200

    add garnix cache

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -12,6 +12,7 @@
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://calops.cachix.org"
+      "https://cache.garnix.io"
     ];
 
     extra-trusted-public-keys = [
@@ -19,6 +20,7 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "calops.cachix.org-1:6RTG80il2oS2ECFeG2QubG+mvD9OJc1s6Lm9JGAFcM0="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
 
     trusted-users = [
