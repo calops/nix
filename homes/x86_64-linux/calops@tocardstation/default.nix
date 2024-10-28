6374ba29edd74923af1be906b65a0170226c6f21
Rémi Labeyrie <calops@tocards.net>
2024-10-28 21:37:24 +0100

    fix font on tocardstation

--- homes/x86_64-linux/calops@tocardstation/default.nix
+++ homes/x86_64-linux/calops@tocardstation/default.nix
@@ -4,6 +4,7 @@
   my.roles.ai.enable = true;
   my.roles.graphical = {
     fonts.monospace = config.my.fonts.iosevka; # TODO: remove once iosevka-comfy is fixed
+    fonts.sizes.terminalCell.width = 1.0;
     installAllFonts = true;
     terminal = "kitty";
     monitors.primary.id = "DP-2";
