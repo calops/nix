2cd453d0d75a43f571277256b62b9d95720a916a
Rémi Labeyrie <calops@tocards.net>
2024-11-06 09:33:56 +0100

    refactor conform

--- modules/home/programs/neovim/config/lua/plugins/formatting.lua
+++ modules/home/programs/neovim/config/lua/plugins/formatting.lua
@@ -21,7 +21,6 @@ return {
 
 			vim.api.nvim_create_user_command("FormatDisable", function(args)
 				if args.bang then
-					---@diagnostic disable-next-line: inject-field
 					vim.b.disable_autoformat = true
 				else
 					vim.g.disable_autoformat = true
@@ -32,43 +31,40 @@ return {
 			})
 
 			vim.api.nvim_create_user_command("FormatEnable", function()
-				---@diagnostic disable-next-line: inject-field
 				vim.b.disable_autoformat = false
 				vim.g.disable_autoformat = false
 			end, {
 				desc = "Re-enable autoformat-on-save",
 			})
 		end,
-		opts = function()
-			return {
-				formatters_by_ft = {
-					javascript = { "prettierd" },
-					typescript = { "prettierd" },
-					json = { "prettierd" },
-					yaml = { "prettierd" },
-					html = { "prettierd" },
+		opts = {
+			formatters_by_ft = {
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				json = { "prettierd" },
+				yaml = { "prettierd" },
+				html = { "prettierd" },
 
-					lua = { "stylua" },
-					nix = { "nixfmt" },
-					python = { "isort", "black" },
-					sh = { "shfmt" },
-					sql = { "sqlfluff" },
+				lua = { "stylua" },
+				nix = { "nixfmt" },
+				python = { "isort", "black" },
+				sh = { "shfmt" },
+				sql = { "sqlfluff" },
+			},
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return {
+					lsp_fallback = true,
+					timeout_ms = 500,
+				}
+			end,
+			formatters = {
+				sqlfluff = {
+					args = { "format", "--dialect=postgres", "-" },
 				},
-				format_on_save = function(bufnr)
-					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-						return
-					end
-					return {
-						lsp_fallback = true,
-						timeout_ms = 500,
-					}
-				end,
-				formatters = {
-					sqlfluff = {
-						args = { "format", "--dialect=postgres", "-" },
-					},
-				},
-			}
-		end,
+			},
+		},
 	},
 }
