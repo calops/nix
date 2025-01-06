64b8eef01f4787b344daf3a0cbafd5755560c5f4
Rémi Labeyrie <calops@tocards.net>
2025-01-06 17:11:18 +0100

    remove airscan

--- modules/nixos/printing.nix
+++ modules/nixos/printing.nix
@@ -10,11 +10,7 @@
   config = lib.mkIf config.my.roles.printing.enable {
     # Scanning
     hardware.sane.enable = true;
-    hardware.sane.extraBackends = [
-      pkgs.sane-airscan
-      pkgs.hplipWithPlugin
-    ];
-    services.udev.packages = [ pkgs.sane-airscan ];
+    hardware.sane.extraBackends = [ pkgs.hplipWithPlugin ];
     users.users.calops.extraGroups = [
       "scanner"
       "lp"
