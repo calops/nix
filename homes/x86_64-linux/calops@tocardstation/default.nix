ce2a1f5544647948015f7dc234bc2673aae2de0c
Rémi Labeyrie <calops@tocards.net>
2025-03-17 20:47:18 +0100

    update everything

--- homes/x86_64-linux/calops@tocardstation/default.nix
+++ homes/x86_64-linux/calops@tocardstation/default.nix
@@ -1,19 +1,15 @@
 {
   config,
-  pkgs,
   ...
 }:
 {
   my.roles.terminal.enable = true;
   my.roles.ai.enable = true;
+
   my.roles.graphical = {
     fonts.monospace = config.my.fonts.aporetic-sans-mono;
     installAllFonts = true;
     terminal = "kitty";
     monitors.primary.id = "DP-2";
   };
-
-  home.packages = [
-    pkgs.rustdesk
-  ];
 }
