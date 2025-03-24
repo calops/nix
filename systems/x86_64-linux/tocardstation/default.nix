524695d8b237c640eb9f2ec0b7b92c77591d182a
Rémi Labeyrie <calops@tocards.net>
2025-03-24 20:04:57 +0100

    rearrange tocardstation hardware

--- systems/x86_64-linux/tocardstation/default.nix
+++ systems/x86_64-linux/tocardstation/default.nix
@@ -8,7 +8,13 @@
   networking.hostName = "tocardstation";
   time.timeZone = "Europe/Paris";
 
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "azerty";
+  };
+
   my.configDir = "/home/calops/nix";
+
   my.roles = {
     graphical.enable = true;
     nvidia.enable = true;
@@ -16,31 +22,47 @@
     audio.enable = true;
     printing.enable = true;
     bluetooth.enable = true;
-    #monitoring.enable = true;
   };
 
-  services.solaar.enable = true;
+  # SSD periodic trimming
+  services.fstrim.enable = true;
+
+  # Logitech mouse and keyboard wireless dongle
   hardware.logitech.wireless.enable = true;
+
+  # Logitech device manager
+  services.solaar.enable = true;
 
   boot = {
     initrd.luks.devices.rootDrive.device = "/dev/disk/by-uuid/ab146bd7-2e99-4aa7-a115-040df4acc43d";
     supportedFilesystems = [ "ntfs" ];
   };
 
-  fileSystems = {
-    "/mnt/stuff" = {
-      device = "/dev/disk/by-uuid/BAC095A2C0956583";
-      fsType = "ntfs";
+  fileSystems =
+    let
+      options = [
+        "noatime"
+        "nodiratime"
+      ];
+    in
+    {
+      "/mnt/stuff" = {
+        inherit options;
+        device = "/dev/disk/by-uuid/BAC095A2C0956583";
+        fsType = "ntfs";
+      };
+      "/mnt/games" = {
+        inherit options;
+        device = "/dev/disk/by-uuid/E084CC7984CC5426";
+        fsType = "ntfs";
+      };
+      "/mnt/data" = {
+        inherit options;
+        device = "/dev/disk/by-uuid/405E8B6F5E8B5C92";
+        fsType = "ntfs";
+      };
+      "/".options = options;
     };
-    "/mnt/games" = {
-      device = "/dev/disk/by-uuid/E084CC7984CC5426";
-      fsType = "ntfs";
-    };
-    "/mnt/data" = {
-      device = "/dev/disk/by-uuid/405E8B6F5E8B5C92";
-      fsType = "ntfs";
-    };
-  };
 
   users.users.calops = {
     isNormalUser = true;
@@ -59,11 +81,6 @@
     "1.1.1.1"
     "1.0.0.1"
   ];
-
-  services.xserver.xkb = {
-    layout = "fr";
-    variant = "azerty";
-  };
 
   # Android virtualisation
   virtualisation.waydroid.enable = true;
