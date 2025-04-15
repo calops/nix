3ade64c7f9e3e9aea050120bf2151108fb9b7a19
Rémi Labeyrie <calops@tocards.net>
2025-04-15 11:56:05 +0200

    fix ssh agent

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -15,7 +15,7 @@
     let
       onePassPath =
         if pkgs.stdenv.isDarwin then
-          "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+          ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"''
         else
           "~/.1password/agent.sock";
     in
