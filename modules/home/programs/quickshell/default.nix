7b343c3e10fb2f2d3f5e679a9a8a5e68ba81e3bf
Rémi Labeyrie <calops@tocards.net>
2025-07-10 22:52:29 +0200

    update flake

--- modules/home/programs/quickshell/default.nix
+++ modules/home/programs/quickshell/default.nix
@@ -4,31 +4,14 @@
   lib,
   ...
 }:
-{
-  options.programs.quickshell = {
-    enable = lib.mkEnableOption "Enable quickshell";
-    package = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.nightly.quickshell;
-      description = ''
-        The quickshell package to use.
-      '';
-    };
+lib.mkIf config.my.roles.graphical.enable {
+  programs.quickshell = {
+    enable = true;
+    activeConfig = null;
+    package = pkgs.nightly.quickshell;
+    systemd.enable = true;
   };
 
-  config =
-    let
-      package = config.programs.quickshell.package;
-    in
-    lib.mkIf config.my.roles.graphical.enable {
-      home.packages = [ package ];
-
-      xdg.configFile."quickshell".source =
-        config.lib.file.mkOutOfStoreSymlink "${config.my.configDir}/modules/home/programs/quickshell/config";
-
-      systemd.user.services.quickshell = lib.my.mkGraphicalSessionService {
-        description = "Quickshell";
-        command = "${lib.getExe' package "quickshell"}";
-      };
-    };
+  xdg.configFile."quickshell".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.my.configDir}/modules/home/programs/quickshell/config";
 }
