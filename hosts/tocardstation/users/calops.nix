60c4c89ed44c6913d707b3622141f87665b64833
Rémi Labeyrie <calops@tocards.net>
2026-03-04 11:44:31 +0100

    fix tocardstation monitor

--- hosts/tocardstation/users/calops.nix
+++ hosts/tocardstation/users/calops.nix
@@ -14,6 +14,13 @@
       name = "Technical Concepts Ltd 34R83Q X2452000226";
       resolution = "3440x1440@170.000";
     };
+    niriExtraConfig = # kdl
+      ''
+        output "Technical Concepts Ltd 34R83Q X2452000226" {
+          mode "3440x1440@170.000"
+          variable-refresh-rate
+        }
+      '';
   };
 
   nix.settings.cores = 22; # keep two cores for the system
