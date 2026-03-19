03009014d69bbc191c5aa382a75960d7773e476e
Rémi Labeyrie <calops@tocards.net>
2026-03-19 17:35:31 +0100

    i know who you are

--- modules/home/programs/sable.nix
+++ modules/home/programs/sable.nix
@@ -6,10 +6,11 @@
 }:
 let
   enabled = config.my.roles.graphical.enable && config.programs.firefoxpwa.enable;
+  profileId = "01KM3AB7E8CC2YRN85SEK03REX";
 in
 {
   config = lib.mkIf enabled {
-    programs.firefoxpwa.profiles."01KM3AB7E8CC2YRN85SEK03REX" = {
+    programs.firefoxpwa.profiles.${profileId} = {
       name = "sable";
 
       sites."01KM38RYD60VZEV7QB5KSK5JYF" = {
@@ -32,5 +33,22 @@ in
       };
     };
 
+    xdg.dataFile = {
+      "firefoxpwa/profiles/${profileId}/user.js".text = ''
+        user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+      '';
+
+      "firefoxpwa/profiles/${profileId}/chrome/userContent.css".text =
+        # css
+        ''
+          body, p, div, span, section {
+              font-family: "Aporetic Sans" !important;
+          }
+
+          code, pre, kbd, samp {
+              font-family: "Aporetic Sans Mono" !important;
+          }
+        '';
+    };
   };
 }
