fcc677eaaec9fb2187ea87856eb846b41aac25d4
Rémi Labeyrie <calops@tocards.net>
2026-07-27 08:06:08 +0200

    fix layouts

--- modules/hardware/nuphy.nix
+++ modules/hardware/nuphy.nix
@@ -2,10 +2,6 @@
 {
   den.aspects.hardware.provides.nuphy = {
     nixos = {
-      services.xserver.xkb = {
-        layout = "fr";
-      };
-
       services.udev.extraRules = ''
         KERNEL=="hidraw*", ATTRS{idVendor}=="19f5", MODE="0666"
       '';
